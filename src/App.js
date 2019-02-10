4603de80d0128697b6a352c3d08f7a4ca8f342e8
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-10 09:18:52 +0700

    create navigation bar using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import logo from './logo.svg';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button }from 'react-bootstrap';
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends Component 
+{
+  render() 
+  {
     return (
       <div className="App">
-        
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home"><img src="logo"/>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          </Navbar>
       </div>
-    );
+      );
   }
 }
 
