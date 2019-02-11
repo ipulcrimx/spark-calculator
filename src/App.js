76ff8205ad6506cb3d269a2818ab168038401cec
Saiful Setyawan <imelgugelkudi@gmail.com>
2019-02-12 01:42:49 +0700

    and remove some class div

--- src/App.js
+++ src/App.js
@@ -11,37 +11,35 @@ class App extends Component
     render()
     {
         return (
-            <div className="main-content">
-                <Container fluid="true">
-                    <Row>
-                        <Col md={3}>
-                            <div className="side-div">
-                                <InputForm />
-                            </div>
-                        </Col>
-                        <Col md={6}>
-                            <Row className="dummy-white">
-                                <Col xl={8}>
-                                    <h2>Spark Calculator</h2>
-                                </Col>
-                                <Col xl={8} className="graph">
-                                    <Graph />
-                                </Col>
-                                <Col xl={8} className="graph">
-                                    <Chart />
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col md={3}>
-                            <div className="side-div">
-                                <p>
-                                    This will be notification or history div
-                                </p>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <Container fluid="true">
+                <Row>
+                    <Col md={3}>
+                        <div className="side-div">
+                            <InputForm />
+                        </div>
+                    </Col>
+                    <Col md={6}>
+                        <Row className="dummy-white">
+                            <Col md="8">
+                                <h2>Spark Calculator</h2>
+                            </Col>
+                            <Col md="12">
+                                <Graph />
+                            </Col>
+                            <Col md="12">
+                                <Chart />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md={3}>
+                        <div className="side-div">
+                            <p>
+                                This will be notification or history div
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
